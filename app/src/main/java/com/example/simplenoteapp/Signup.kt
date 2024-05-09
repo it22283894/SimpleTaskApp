96e9766bb5be53a8e2878cf67260c8b7172e360e
it22283894 <128390727+it22283894@users.noreply.github.com>
2024-05-10 00:11:42 +0530

    commit9

--- app/src/main/java/com/example/simplenoteapp/Signup.kt
+++ app/src/main/java/com/example/simplenoteapp/Signup.kt
@@ -4,11 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import com.example.simplenoteapp.databinding.ActivitySignupBinding
 
 class Signup : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var databaseHelper: DatabaseLS
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        binding=ActivitySignupBinding.inflate(layoutInflater)
+        databaseHelper= DatabaseLS(this)
 
         val textView24=findViewById<TextView>(R.id.textView24)
 
@@ -17,5 +25,17 @@ class Signup : AppCompatActivity() {
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
         }
+    }
+    private fun signupDatabase(username:String,password:String,phone:Int){
+        val insertRowId=databaseHelper.insertUser(username,password,phone)
+        if (insertRowId !=-1L){
+            Toast.makeText(this,"Signup Successfull!!",Toast.LENGTH_SHORT).show()
+            val intent=Intent(this,Login::class.java)
+            startActivity(intent)
+            finish()
+        }else{
+            Toast.makeText(this,"Signup Failed!!",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
