b02c8010c949223b6383774ccc114a952afab059
it22283894 <128390727+it22283894@users.noreply.github.com>
2024-05-09 15:15:18 +0530

    commit3

--- app/src/main/java/com/example/simplenoteapp/MainActivity.kt
+++ app/src/main/java/com/example/simplenoteapp/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.simplenoteapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //main
+
+        val delayMillis: Long = 3000
+
+        // Post a delayed action to navigate to the next activity
+        Handler().postDelayed({
+            val intent = Intent(this, AddNote::class.java)
+            startActivity(intent)
+            finish() // Optional: finish the current activity to prevent going back to it
+        }, delayMillis)
     }
 }
