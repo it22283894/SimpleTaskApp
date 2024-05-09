e7836c8921ea4cd84a5cbf62b1b7d6fe78a66ce0
it22283894 <128390727+it22283894@users.noreply.github.com>
2024-05-09 16:51:23 +0530

    commit5

--- app/src/main/java/com/example/simplenoteapp/onBoard1.kt
+++ app/src/main/java/com/example/simplenoteapp/onBoard1.kt
@@ -1,11 +1,21 @@
 package com.example.simplenoteapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class onBoard1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_board1)
+
+        val textView6=findViewById<TextView>(R.id.textView6)
+
+        textView6.setOnClickListener {
+            // Create an Intent to navigate to the next activity
+            val intent = Intent(this, onBoard2::class.java)
+            startActivity(intent)
+        }
     }
 }
