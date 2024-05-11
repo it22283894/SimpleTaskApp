c9b17b362184da990563a9b5e6ee57d06316a93a
it22283894 <128390727+it22283894@users.noreply.github.com>
2024-05-11 16:28:27 +0530

    commit15

--- app/src/main/java/com/example/simplenoteapp/Update.kt
+++ app/src/main/java/com/example/simplenoteapp/Update.kt
@@ -2,10 +2,41 @@ package com.example.simplenoteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.simplenoteapp.databinding.ActivityUpdateBinding
 
 class Update : AppCompatActivity() {
+    private lateinit var binding:ActivityUpdateBinding
+    private lateinit var db:NoteDatabaseHelper
+    private var noteId:Int = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_update)
+        binding = ActivityUpdateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NoteDatabaseHelper(this)
+
+        noteId = intent.getIntExtra("note_id", -1)
+        if (noteId == -1) {
+            finish()
+            return
+        }
+
+        val note = db.getNoteById(noteId)
+        if (note != null) {
+            binding.updateTitleEditText.setText(note.title)
+            binding.updateContentEditText.setText(note.content)
+        }
+
+        binding.updateSave.setOnClickListener {
+            val newTitle = binding.updateTitleEditText.text.toString()
+            val newContent = binding.updateContentEditText.text.toString()
+            val updateNote = Note(noteId, newTitle, newContent)
+            db.updateNote(updateNote)
+            finish()
+            Toast.makeText(this, "Changes Saved", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
